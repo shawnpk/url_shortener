4c548b1a526191dd15fd73ab46e66c3ac7245fc0
Shawn Kearney <shawnpk29@me.com>
2019-05-17 13:10:26 -0600

    Implement ability for short links to redirect to original long urls

--- app/controllers/shorten_urls_controller.rb
+++ app/controllers/shorten_urls_controller.rb
@@ -1,6 +1,17 @@
 class ShortenUrlsController < ApplicationController
   include ShortenUrlActions
 
+  def show
+    url = ShortenUrl.find_by(short_link: params[:short_link])
+
+    if url
+      url.save
+      redirect_to url.body
+    else
+      render json: nil, status: :no_content
+    end
+  end
+
   def create
     render json: shorten_link(params[:url])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/short_link', to: 'shorten_urls#create', as: :create_short_link
+  get '/:short_link',     to: "shorten_urls#show",   as: "redirect_link"
 end
